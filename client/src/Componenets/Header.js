05fc54adf1623f9d02ebbc9d484a33eca6182ef7
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-04-23 14:24:36 +0100

    Update Header.js PopUp

--- client/src/Componenets/Header.js
+++ client/src/Componenets/Header.js
@@ -8,6 +8,7 @@ import back from "../BGSITE.png"
 import Pop from "../../src/pop.png"
 import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 import { FaShoppingCart } from "react-icons/fa";
+import vf from "./ImagePUB1.png"
 
 function Header() {
   const totalQte = useSelector((state)=>state.cart.cartTotalQuantity)
@@ -630,21 +631,21 @@ function Header() {
 
     </p>
   </div>
-  {/* <Modal show={showPopup} onHide={handleClose}>
+  <Modal show={showPopup} onHide={handleClose}>
   <Modal.Body>
         <Modal.Header closeButton>
           <p>Offre valable de Vendredi 12 jusqu'à Jeudi 18 Janvier 2024</p>
         </Modal.Header>
         <Modal.Body>
-  <img src={Pop} alt="" style={{ width: '100%', height: 'auto', maxWidth: '100%', maxHeight: '150%' }} />
+  <img src={vf} alt="" style={{ width: '100%', height: 'auto', maxWidth: '100%', maxHeight: '150%' }} />
 </Modal.Body>
         <Modal.Footer style={{ justifyContent: 'flex-start' }}>
-  <Button href='http://localhost:3000/product/MOSAIQUE01' variant="primary" onClick={handleClose} style={{ backgroundColor: '#e51d4e', border: 'none' }}>
+  <Button  variant="primary" onClick={handleClose} style={{ backgroundColor: '#e51d4e', border: 'none' }}>
     Acheter maintenant
   </Button>
 </Modal.Footer>
 </Modal.Body>
-      </Modal> */}
+      </Modal> 
 </div>
 
 
