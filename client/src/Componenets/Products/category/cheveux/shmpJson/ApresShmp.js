7d4c462b229b93d434c17e5fa33c3127c5f5e137
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-05-02 13:05:36 +0100

    Update ApresShmp.js c05 .

--- client/src/Componenets/Products/category/cheveux/shmpJson/ApresShmp.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/ApresShmp.js
@@ -3,7 +3,7 @@ const ApresShmp = [
     "OffersNo": 1486513,
     "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Après-shampooing 250ml",
     "category": "Cheveux ",
-    "category": "Après-Shampooing",
+    "Column4": "Après-Shampooing",
     "PrixRegulier": 22,
     "PrixResultatOffre": 13
    },
@@ -11,7 +11,7 @@ const ApresShmp = [
     "OffersNo": 1375107,
     "DescriptionOffre": "Advance techniques Ultimate Shine : Après-shampooing 250ml",
     "category": "Cheveux ",
-    "category": "Après-Shampooing",
+    "Column4": "Après-Shampooing",
     "PrixRegulier": 22,
     "PrixResultatOffre": 13
    },
@@ -19,7 +19,7 @@ const ApresShmp = [
     "OffersNo": 1486516,
     "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Après-shampooing 250ml",
     "category": "Cheveux ",
-    "category": "Après-Shampooing",
+    "Column4": "Après-Shampooing",
     "PrixRegulier": 22,
     "PrixResultatOffre": 13
    },
@@ -27,9 +27,25 @@ const ApresShmp = [
     "OffersNo": 1499372,
     "DescriptionOffre": "Deeply Purifying : Après-Shampooing Purifiant 250ml",
     "category": "Cheveux ",
-    "category": "Après-Shampooing",
+    "Column4": "Après-Shampooing",
     "PrixRegulier": 22,
     "PrixResultatOffre": 13
    },
+   {
+    "OffersNo": 1486507,
+    "DescriptionOffre": "Advance techniques Loss Control : Après-shampooing 250ml",
+    "category": "Cheveux ",
+    "Column4": "Après-Shampooing",
+    "PrixRegulier": 22,
+    "PrixResultatOffre": 13
+   },
+   {
+    "OffersNo": 1480994,
+    "DescriptionOffre": "Advance techniques Hydra Boost : Après-Shampooing 250ml",
+    "category": "Cheveux ",
+    "Column4": "Après-Shampooing",
+    "PrixRegulier": 22,
+    "PrixResultatOffre": 13
+   }
 ]
 export default ApresShmp
