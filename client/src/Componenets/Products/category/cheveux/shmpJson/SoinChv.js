cb6c42e5de7e33000a71e82e849f9ed58e86ce7c
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-05-02 13:06:09 +0100

    Update SoinChv.js

--- client/src/Componenets/Products/category/cheveux/shmpJson/SoinChv.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/SoinChv.js
@@ -1,51 +1,58 @@
-const SoinChv = [
-  {
-    "OffersNo": 1371660,
-    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Sérum 30ml",
-    "category": "Cheveux ",
-    "category": "Soin",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 17
-   },
-   {
-    "OffersNo": 1374381,
-    "DescriptionOffre": "Advance techniques Loss Control : Soin 100ml",
-    "category": "Cheveux ",
-    "category": "Soin",
-    "PrixRegulier": 36,
-    "PrixResultatOffre": 20.5
-   },
-   {
-    "OffersNo": 1456698,
-    "DescriptionOffre": "Advance techniques Fabulous Curls : Crème de Coiffage 150ml",
-    "category": "Cheveux ",
-    "category": "Soin",
-    "PrixRegulier": 22,
-    "PrixResultatOffre": 13
-   },
-   {
-    "OffersNo": 1502290,
-    "DescriptionOffre": "Deeply Purifying : Gommage Exfoliant Purifiant 200ml",
-    "category": "Cheveux ",
-    "category": "Soin",
-    "PrixRegulier": 48,
-    "PrixResultatOffre": 27
-   },
-   {
-    "OffersNo": 1374389,
-    "DescriptionOffre": "Advance techniques Ultimate Shine : Sérum 30ml",
-    "category": "Cheveux ",
-    "category": "Soin",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 21
-   },
-   {
-    "OffersNo": 1371657,
-    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Huile 100ml",
-    "category": "Cheveux ",
-    "category": "Soin",
-    "PrixRegulier": 40,
-    "PrixResultatOffre": 24
-   },
+const SoinChv = [ {
+  "OffersNo": 1371660,
+  "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Sérum 30ml",
+  "category": "Cheveux ",
+  "Column4": "Soin",
+  "PrixRegulier": 35,
+  "PrixResultatOffre": 20
+ },
+ {
+  "OffersNo": 1371657,
+  "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Huile 100ml",
+  "category": "Cheveux ",
+  "Column4": "Soin",
+  "PrixRegulier": 40,
+  "PrixResultatOffre": 23
+ },
+ {
+  "OffersNo": 1374389,
+  "DescriptionOffre": "Advance techniques Ultimate Shine : Sérum 30ml",
+  "category": "Cheveux ",
+  "Column4": "Soin",
+  "PrixRegulier": 35,
+  "PrixResultatOffre": 20
+ },
+ {
+  "OffersNo": 1456698,
+  "DescriptionOffre": "Advance techniques Fabulous Curls : Crème de Coiffage 150ml",
+  "category": "Cheveux ",
+  "Column4": "Soin",
+  "PrixRegulier": 22,
+  "PrixResultatOffre": 12.5
+ },
+ {
+  "OffersNo": 1502290,
+  "DescriptionOffre": "Deeply Purifying : Gommage Exfoliant Purifiant 200ml",
+  "category": "Cheveux ",
+  "Column4": "Soin",
+  "PrixRegulier": 48,
+  "PrixResultatOffre": 23
+ },
+ {
+  "OffersNo": 1480989,
+  "DescriptionOffre": "Advance techniques Hydra Boost : Soin",
+  "category": "Cheveux ",
+  "Column4": "Soin",
+  "PrixRegulier": 30,
+  "PrixResultatOffre": 14
+ },
+ {
+  "OffersNo": 1533602,
+  "DescriptionOffre": "Advance Technique Loss Control Spray 100ml",
+  "category": "Cheveux ",
+  "Column4": "Soin",
+  "PrixRegulier": 36,
+  "PrixResultatOffre": 21
+ }
 ]
 export default SoinChv
