6a308acb85d7b22397a022262e1b1ed8d1146ba9
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-05-02 12:39:48 +0100

    Update Svc.js svc 05

--- client/src/Componenets/Products/category/femme/JsonFemme/Svc.js
+++ client/src/Componenets/Products/category/femme/JsonFemme/Svc.js
@@ -1,6 +1,6 @@
 const Svc = [ 
 
-  
+
   {
     "OffersNo": 1344721,
     "DescriptionOffre": "Anew Nettoyant : Tonique éclaircissant 200ml",
@@ -154,13 +154,164 @@ const Svc = [
     "PrixResultatOffre": 57
    },
    {
+    "OffersNo": 1344721,
+    "DescriptionOffre": "Anew Nettoyant : Tonique éclaircissant 200ml",
+    "category": "Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 66,
+    "PrixResultatOffre": 33
+   },
+   {
+    "OffersNo": 1344969,
+    "DescriptionOffre": "Anew Nettoyant : Eau Micellaire 200ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 66,
+    "PrixResultatOffre": 33
+   },
+   {
+    "OffersNo": 1334439,
+    "DescriptionOffre": "Anew Nettoyant : Gel Nettoyant 150ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 60,
+    "PrixResultatOffre": 30
+   },
+   {
+    "OffersNo": 1440423,
+    "DescriptionOffre": "Anew Protinol : Sérum Anti-âge 30ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 132,
+    "PrixResultatOffre": 66
+   },
+   {
+    "OffersNo": 1387168,
+    "DescriptionOffre": "Anew Vitamine C :  Sérum Eclat 30ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 132,
+    "PrixResultatOffre": 79
+   },
+   {
+    "OffersNo": 1499373,
+    "DescriptionOffre": "HA+ Hyaluronic Acid Complex : Concentré Hydratant Repulpant 30ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 132,
+    "PrixResultatOffre": 66
+   },
+   {
+    "OffersNo": 1499628,
+    "DescriptionOffre": "Anew Platinum : Sérum Liftant et Raffermissant 30ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 132,
+    "PrixResultatOffre": 66
+   },
+   {
+    "OffersNo": 1531425,
+    "DescriptionOffre": "Anew Système Dual : Lissant pour les yeux 10ml*2",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 108,
+    "PrixResultatOffre": 54
+   },
+   {
+    "OffersNo": 1488242,
+    "DescriptionOffre": "Anew Sensitive+ : Crème yeux 15ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 62,
+    "PrixResultatOffre": 31
+   },
+   {
+    "OffersNo": 1499626,
+    "DescriptionOffre": "Anew Protinol : Renewal Power : Crème Contour des yeux 15ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 62,
+    "PrixResultatOffre": 31
+   },
+   {
+    "OffersNo": 1533934,
+    "DescriptionOffre": "Anew Vitamine C : Crème pour les yeux correction éclat et cernes 15ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 62,
+    "PrixResultatOffre": 37
+   },
+   {
+    "OffersNo": 1469186,
+    "DescriptionOffre": "Anew Reversalist : Crème perfectrice de jour SPF 25 50ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 132,
+    "PrixResultatOffre": 66
+   },
+   {
+    "OffersNo": 1387108,
+    "DescriptionOffre": "Anew Reversalist : Crème de nuit 50ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 132,
+    "PrixResultatOffre": 66
+   },
+   {
+    "OffersNo": 1387105,
+    "DescriptionOffre": "Anew Ultimate : Crème de jour SPF 25 50ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 152,
+    "PrixResultatOffre": 76
+   },
+   {
+    "OffersNo": 1387103,
+    "DescriptionOffre": "Anew Ultimate : Crème de nuit 50ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 152,
+    "PrixResultatOffre": 76
+   },
+   {
+    "OffersNo": 1387101,
+    "DescriptionOffre": "Anew Platinum : Crème de jour SPF 25 50ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 158,
+    "PrixResultatOffre": 79
+   },
+   {
+    "OffersNo": 1387102,
+    "DescriptionOffre": "Anew Platinum : Crème de nuit 50ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 158,
+    "PrixResultatOffre": 79
+   },
+   {
+    "OffersNo": 1389430,
+    "DescriptionOffre": "Anew Platinum : Masque Pelliculable à base d'argent 75ml",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 58,
+    "PrixResultatOffre": 29
+   },
+   {
+    "OffersNo": 1507670,
+    "DescriptionOffre": "Anew HA+ Hyaluronic Acid Complex : Masque de nuit Hydratant  ",
+    "category": "Homme et Femme",
+    "Column4": "Soins du visage",
+    "PrixRegulier": 114,
+    "PrixResultatOffre": 57
+   },
+   {
     "OffersNo": 1509892,
     "DescriptionOffre": "Anew Vitamine C Microdermabrasion : Gommage pour le visage 75ml",
     "category": "Homme et Femme",
-    "category": "Soins du visage",
+    "Column4": "Soins du visage",
     "PrixRegulier": 84,
     "PrixResultatOffre": 42
    }
-
     ]
   export default Svc
