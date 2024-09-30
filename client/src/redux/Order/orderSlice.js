f6a43cb6be3267fff7dafb1c3d4ae0a7fe4a7eea
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-09-30 14:31:25 +0100

    Update orderSlice.js Update Backend Running

--- client/src/redux/Order/orderSlice.js
+++ client/src/redux/Order/orderSlice.js
@@ -8,7 +8,7 @@ export const createOrder = createAsyncThunk(
   async (orderData, thunkAPI) => {
     try {
       await axios.post(
-        "https://prod-back.avon-tunisie-shop.com.tn/api/orders/create-order",
+        "https://http://51.222.25.51:5000/api/orders/create-order",
         orderData
       );
       toast.success("Commande créée avec succès", {
