8955c36c9cd7f8ef8fdc5e0e21b63a934e190cb6
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-10-01 14:30:39 +0100

    Update Shmp.js shmp C10

--- client/src/Componenets/Products/category/cheveux/shmpJson/Shmp.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/Shmp.js
@@ -1,113 +1,119 @@
 const Chmp = [
-  {
-    "OffersNo": 1472533,
-    "DescriptionOffre": "Nourish & Smooth : Shampooing 2en1 700ml",
+  
+   {
+    "OffersNo": 1551367,
+    "DescriptionOffre": "Advance techniques Mega Volume : Shampooing 400ml ",
     "category": "Cheveux ",
     "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 19
+    "PrixRegulier": 32,
+    "PrixResultatOffre": 15.5,
+    "Column8": 16.5
    },
- 
+
    {
     "OffersNo": 1374387,
     "DescriptionOffre": "Advance techniques Ultimate Shine : Shampooing 400ml ",
     "category": "Cheveux ",
     "Column4": "Shampooing",
     "PrixRegulier": 32,
-    "PrixResultatOffre": 20
+    "PrixResultatOffre": 18.5,
+    "Column8": 13.5
    },
 
-  
-  
-   {
-    "OffersNo": 1390758,
-    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Shampooing 700ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 38,
-    "PrixResultatOffre": 18
-   },
-   {
-    "OffersNo": 1390759,
-    "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Shampooing 700ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 38,
-    "PrixResultatOffre": 18
-   },
-   {
-    "OffersNo": 1390760,
-    "DescriptionOffre": "Advance techniques Ultimate Shine : Shampooing 700ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 38,
-    "PrixResultatOffre": 18
-   },
-   {
-    "OffersNo": 1374392,
-    "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Shampooing 400ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 20
-   },
-   {
-    "OffersNo": 1374342,
-    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Shampooing 400ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 20
-   },
-   {
-    "OffersNo": 1499371,
-    "DescriptionOffre": "Deeply Purifying : Shampooing Purifiant 400ml",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 20
-   },
-   {
-    "OffersNo": 1472536,
-    "DescriptionOffre": "Healthy Hydratation : Shampooing 2en1 700ml",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 19
-   },
-   {
-    "OffersNo": 1472531,
-    "DescriptionOffre": "Stay Strong : Shampooing 2en1 700ml",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 19
-   },
-   {
-    "OffersNo": 1472534,
-    "DescriptionOffre": "Refresh & Revitalise : Shampooing 2en1 700ml",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 19
-   },
-   {
-    "OffersNo": 1472532,
-    "DescriptionOffre": "Full Volume : Shampooing 2en1 700ml",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 19
-   },
    {
     "OffersNo": 1480990,
     "DescriptionOffre": "Advance techniques Hydra Boost : Shampooing 400ml",
     "category": "Cheveux ",
     "Column4": "Shampooing",
     "PrixRegulier": 32,
-    "PrixResultatOffre": 20
+    "PrixResultatOffre": 18.5,
+    "Column8": 13.5
+   },
+ 
+   {
+    "OffersNo": 1499371,
+    "DescriptionOffre": "Advance techniques Deeply Purifying : Shampooing 400ml ",
+    "category": "Cheveux ",
+    "Column4": "Shampooing",
+    "PrixRegulier": 32,
+    "PrixResultatOffre": 18.5,
+    "Column8": 13.5
    },
 
-
+   {
+    "OffersNo": 1374342,
+    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Shampooing 400ml ",
+    "category": "Cheveux ",
+    "Column4": "Shampooing",
+    "PrixRegulier": 32,
+    "PrixResultatOffre": 18.5,
+    "Column8": 13.5
+   },
+ 
+   {
+    "OffersNo": 1374392,
+    "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Shampooing 400ml ",
+    "category": "Cheveux ",
+    "Column4": "Shampooing",
+    "PrixRegulier": 32,
+    "PrixResultatOffre": 18.5,
+    "Column8": 13.5
+   },
+   {
+    "OffersNo": 1486516,
+    "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Après-shampooing 250ml",
+    "category": "Cheveux ",
+    "Column4": "Après-Shampooing",
+    "PrixRegulier": 22,
+    "PrixResultatOffre": 12.5,
+    "Column8": 9.5
+   },
+ 
+ 
+   {
+    "OffersNo": 1472531,
+    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Stay Strong ",
+    "category": "Cheveux ",
+    "Column4": "Shampooing",
+    "PrixRegulier": 35,
+    "PrixResultatOffre": 21,
+    "Column8": 14
+   },
+   {
+    "OffersNo": 1472536,
+    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Healthy Hydration ",
+    "category": "Cheveux ",
+    "Column4": "Shampooing",
+    "PrixRegulier": 35,
+    "PrixResultatOffre": 21,
+    "Column8": 14
+   },
+   {
+    "OffersNo": 1472534,
+    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Refresh Revitalize",
+    "category": "Cheveux ",
+    "Column4": "Shampooing",
+    "PrixRegulier": 35,
+    "PrixResultatOffre": 21,
+    "Column8": 14
+   },
+   {
+    "OffersNo": 1472533,
+    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Nourish & Smooth",
+    "category": "Cheveux ",
+    "Column4": "Shampooing",
+    "PrixRegulier": 35,
+    "PrixResultatOffre": 21,
+    "Column8": 14
+   },
+   {
+    "OffersNo": 1472532,
+    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Full Volume",
+    "category": "Cheveux ",
+    "Column4": "Shampooing",
+    "PrixRegulier": 35,
+    "PrixResultatOffre": 21,
+    "Column8": 14
+   }
 ]
 export default Chmp
