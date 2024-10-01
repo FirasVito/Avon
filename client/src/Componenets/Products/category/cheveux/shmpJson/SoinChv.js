59b86cadda5d883aa643c65da0bdfb520fbc9392
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-10-01 14:31:52 +0100

    Update SoinChv.js SoinChvx c10

--- client/src/Componenets/Products/category/cheveux/shmpJson/SoinChv.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/SoinChv.js
@@ -1,42 +1,12 @@
 const SoinChv = [ 
   {
-    "OffersNo": 1480989,
-    "DescriptionOffre": "Advance techniques Hydra Boost : Soin",
-    "category": "Cheveux ",
-    "Column4": "Soin",
-    "PrixRegulier": 30,
-    "PrixResultatOffre": 18
-   },
-   {
-    "OffersNo": 1533602,
-    "DescriptionOffre": "Advance Technique Loss Control Spray 100ml",
-    "category": "Cheveux ",
-    "Column4": "Soin",
-    "PrixRegulier": 36,
-    "PrixResultatOffre": 21
-   },
-    {
     "OffersNo": 1456698,
     "DescriptionOffre": "Advance techniques Fabulous Curls : Crème de Coiffage 150ml",
     "category": "Cheveux ",
     "Column4": "Soin",
     "PrixRegulier": 22,
-    "PrixResultatOffre": 13
-   },
-   {
-    "OffersNo": 1502290,
-    "DescriptionOffre": "Deeply Purifying : Gommage Exfoliant Purifiant 200ml",
-    "category": "Cheveux ",
-    "Column4": "Soin",
-    "PrixRegulier": 48,
-    "PrixResultatOffre": 28
-   },   {
-    "OffersNo": 1374389,
-    "DescriptionOffre": "Advance techniques Ultimate Shine : Sérum 30ml",
-    "category": "Cheveux ",
-    "Column4": "Soin",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 21
+    "PrixResultatOffre": 12.5,
+    "Column8": 9.5
    },
    {
     "OffersNo": 1371660,
@@ -44,7 +14,8 @@ const SoinChv = [
     "category": "Cheveux ",
     "Column4": "Soin",
     "PrixRegulier": 35,
-    "PrixResultatOffre": 21
+    "PrixResultatOffre": 20,
+    "Column8": 15
    },
    {
     "OffersNo": 1371657,
@@ -52,8 +23,36 @@ const SoinChv = [
     "category": "Cheveux ",
     "Column4": "Soin",
     "PrixRegulier": 40,
-    "PrixResultatOffre": 24
+    "PrixResultatOffre": 23,
+    "Column8": 17
    },
-
+   {
+    "OffersNo": 1480989,
+    "DescriptionOffre": "Advance techniques Hydra Boost : Soin",
+    "category": "Cheveux ",
+    "Column4": "Soin",
+    "PrixRegulier": 30,
+    "PrixResultatOffre": 17,
+    "Column8": 13
+   },
+     
+   {
+    "OffersNo": 1374389,
+    "DescriptionOffre": "Advance techniques Ultimate Shine : Sérum 30ml",
+    "category": "Cheveux ",
+    "Column4": "Soin",
+    "PrixRegulier": 35,
+    "PrixResultatOffre": 20,
+    "Column8": 15
+   },
+   {
+    "OffersNo": 1551369,
+    "DescriptionOffre": "Advance techniques Mega Volume : Soin en Vaporisateur 100ml ",
+    "category": "Cheveux ",
+    "Column4": "Soin",
+    "PrixRegulier": 32,
+    "PrixResultatOffre": 15.5,
+    "Column8": 16.5
+   },
 ]
 export default SoinChv
