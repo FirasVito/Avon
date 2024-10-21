681d10bdc7080ac1272b2b94acd79947212624c0
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-10-21 09:57:54 +0100

    Update Enf.js

--- client/src/Componenets/Products/category/enfants/Enf.js
+++ client/src/Componenets/Products/category/enfants/Enf.js
@@ -1,54 +1,6 @@
 const Enf = [
 
-  {
-    "OffersNo": 1514158,
-    "DescriptionOffre": "Avon Girls Rule : Laits pour le Corps 250ml ",
-    "category": "Enfants ",
-    "Column4": "Enfants",
-    "PrixRegulier": 30,
-    "PrixResultatOffre": 17
-   },
-   {
-    "OffersNo": 1514157,
-    "DescriptionOffre": "Avon Girls Rule : Brume pour le Corps 100ml",
-    "category": "Enfants ",
-    "Column4": "Enfants",
-    "PrixRegulier": 24,
-    "PrixResultatOffre": 14
-   },
-   {
-    "OffersNo": 1514156,
-    "DescriptionOffre": "Avon Girls Rule : Gel Douche 250ml ",
-    "category": "Enfants ",
-    "Column4": "Enfants",
-    "PrixRegulier": 24,
-    "PrixResultatOffre": 14
-   },
-   {
-    "OffersNo": 1577000,
-    "DescriptionOffre": "Naturals Kids : Spray cheveux Mangue 200ml",
-    "category": "Enfants ",
-    "Column4": "Enfants",
-    "PrixRegulier": 28,
-    "PrixResultatOffre": 16
-   },
-   {
-    "OffersNo": 1393600,
-    "DescriptionOffre": "Naturals Kids : Shampooing et après Shampooing Pomme 250ml",
-    "category": "Enfants ",
-    "Column4": "Enfants",
-    "PrixRegulier": 28,
-    "PrixResultatOffre": 16
-   },
- //  {
-  //   "OffersNo": 328100,
-  //   "DescriptionOffre": "Avon Naturals Kids: Gel Douche et Bain Mousse Lavande 250ml",
-  //   "category": "Enfants ",
-  //   "Column4": "Enfants",
-  //   "PrixRegulier": 28,
-  //   "PrixResultatOffre": 16
-  //  }
-
+ 
 ];
 
 export default Enf;
