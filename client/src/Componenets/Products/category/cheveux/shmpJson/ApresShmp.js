2eaf1537cd59f667f04bb182279fef9e1341865c
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-11-11 14:58:20 +0100

    Update ApresShmp.js

--- client/src/Componenets/Products/category/cheveux/shmpJson/ApresShmp.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/ApresShmp.js
@@ -1,49 +1,19 @@
 const ApresShmp = [
   {
-    "OffersNo": 1480994,
-    "DescriptionOffre": "Advance techniques Hydra Boost : Après-Shampooing 250ml",
-    "category": "Cheveux ",
-    "Column4": "Après-Shampooing",
-    "PrixRegulier": 22,
-    "PrixResultatOffre": 12.5,
-    "Column8": 9.5
-   },
-
-   {
     "OffersNo": 1551368,
     "DescriptionOffre": "Advance techniques Mega Volume : Après-shampooing 250ml ",
     "category": "Cheveux ",
     "Column4": "Après-Shampooing",
     "PrixRegulier": 22,
-    "PrixResultatOffre": 10.5,
-    "Column8": 11.5
+    "PrixResultatOffre": 14
    },
    {
-    "OffersNo": 1375107,
-    "DescriptionOffre": "Advance techniques Ultimate Shine : Après-shampooing 250ml",
+    "OffersNo": 1486516,
+    "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Après-shampooing 250ml",
     "category": "Cheveux ",
     "Column4": "Après-Shampooing",
     "PrixRegulier": 22,
-    "PrixResultatOffre": 12.5,
-    "Column8": 9.5
-   },
-   {
-    "OffersNo": 1499372,
-    "DescriptionOffre": "Advance techniques Deeply Purifying : Après-Shampooing 250ml ",
-    "category": "Cheveux ",
-    "Column4": "Après-Shampooing",
-    "PrixRegulier": 22,
-    "PrixResultatOffre": 12.5,
-    "Column8": 9.5
-   },
-   {
-    "OffersNo": 1486513,
-    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Après-shampooing 250ml",
-    "category": "Cheveux ",
-    "Column4": "Après-Shampooing",
-    "PrixRegulier": 22,
-    "PrixResultatOffre": 12.5,
-    "Column8": 9.5
-   },
+    "PrixResultatOffre": 14
+   }
 ]
 export default ApresShmp
