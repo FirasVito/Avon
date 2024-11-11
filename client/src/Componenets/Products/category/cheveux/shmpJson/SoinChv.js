bfb3f15092eea1cfd9abc7163ef2ed46da7fb0f5
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-11-11 14:55:56 +0100

    Update SoinChv.js

--- client/src/Componenets/Products/category/cheveux/shmpJson/SoinChv.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/SoinChv.js
@@ -1,60 +1,68 @@
 const SoinChv = [ 
+
   {
-    "OffersNo": 1456698,
-    "DescriptionOffre": "Advance techniques Fabulous Curls : Crème de Coiffage 150ml",
+    "OffersNo": 1551369,
+    "DescriptionOffre": "Advance techniques Mega Volume : Soin en Vaporisateur 100ml ",
     "category": "Cheveux ",
-    "Column4": "Soin",
-    "PrixRegulier": 22,
-    "PrixResultatOffre": 12.5,
-    "Column8": 9.5
-   },
-  
-  
-   {
-    "OffersNo": 1371660,
-    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Sérum 30ml",
-    "category": "Cheveux ",
-    "Column4": "Soin",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 20,
-    "Column8": 15
-   },
-   {
-    "OffersNo": 1371657,
-    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Huile 100ml",
-    "category": "Cheveux ",
-    "Column4": "Soin",
-    "PrixRegulier": 40,
-    "PrixResultatOffre": 23,
-    "Column8": 17
+    "Column4": "Soins",
+    "PrixRegulier": 32,
+    "PrixResultatOffre": 20
    },
    {
     "OffersNo": 1480989,
     "DescriptionOffre": "Advance techniques Hydra Boost : Soin",
     "category": "Cheveux ",
-    "Column4": "Soin",
+    "Column4": "Soins",
     "PrixRegulier": 30,
-    "PrixResultatOffre": 17,
-    "Column8": 13
+    "PrixResultatOffre": 15
    },
-     
+   {
+    "OffersNo": 1456698,
+    "DescriptionOffre": "Advance techniques Fabulous Curls : Crème de Coiffage 150ml",
+    "category": "Cheveux ",
+    "Column4": "Soins",
+    "PrixRegulier": 22,
+    "PrixResultatOffre": 11
+   },
+   {
+    "OffersNo": 1371660,
+    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Sérum 30ml",
+    "category": "Cheveux ",
+    "Column4": "Soins",
+    "PrixRegulier": 35,
+    "PrixResultatOffre": 22
+   },
    {
     "OffersNo": 1374389,
     "DescriptionOffre": "Advance techniques Ultimate Shine : Sérum 30ml",
     "category": "Cheveux ",
-    "Column4": "Soin",
+    "Column4": "Soins",
     "PrixRegulier": 35,
-    "PrixResultatOffre": 20,
-    "Column8": 15
+    "PrixResultatOffre": 22
    },
    {
-    "OffersNo": 1551369,
-    "DescriptionOffre": "Advance techniques Mega Volume : Soin en Vaporisateur 100ml ",
+    "OffersNo": 1552014,
+    "DescriptionOffre": "Avon Kids Little Monster : Mangue Spray démêlant pour cheveux 200ml",
     "category": "Cheveux ",
-    "Column4": "Soin",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 15.5,
-    "Column8": 16.5
+    "Column4": "Soins",
+    "PrixRegulier": 30,
+    "PrixResultatOffre": 15
+   },
+   {
+    "OffersNo": 1550728,
+    "DescriptionOffre": "Avon Kids Fresh Scent : Gel pour cheveux 50ml",
+    "category": "Cheveux ",
+    "Column4": "Soins",
+    "PrixRegulier": 30,
+    "PrixResultatOffre": 15
+   },
+   {
+    "OffersNo": 1550726,
+    "DescriptionOffre": "Avon Kids Floral Scent : Spray démêlant pour cheveux 200ml",
+    "category": "Cheveux ",
+    "Column4": "Soins",
+    "PrixRegulier": 30,
+    "PrixResultatOffre": 15
    },
 ]
 export default SoinChv
