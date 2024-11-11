a51750639702f7098051215b9edcc98232421da3
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-11-11 14:53:09 +0100

    Update Shmp.js

--- client/src/Componenets/Products/category/cheveux/shmpJson/Shmp.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/Shmp.js
@@ -1,120 +1,68 @@
 const Chmp = [
   
    {
-    "OffersNo": 1551367,
-    "DescriptionOffre": "Advance techniques Mega Volume : Shampooing 400ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 15.5,
-    "Column8": 16.5
-   },
-
-
-   {
-    "OffersNo": 1374387,
-    "DescriptionOffre": "Advance techniques Ultimate Shine : Shampooing 400ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 18.5,
-    "Column8": 13.5
-   },
-
-   {
-    "OffersNo": 1480990,
-    "DescriptionOffre": "Advance techniques Hydra Boost : Shampooing 400ml",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 18.5,
-    "Column8": 13.5
-   },
- 
-   {
-    "OffersNo": 1499371,
-    "DescriptionOffre": "Advance techniques Deeply Purifying : Shampooing 400ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 18.5,
-    "Column8": 13.5
-   },
-
-   {
-    "OffersNo": 1374342,
-    "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Shampooing 400ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 18.5,
-    "Column8": 13.5
-   },
- 
-   {
-    "OffersNo": 1374392,
-    "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Shampooing 400ml ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 32,
-    "PrixResultatOffre": 18.5,
-    "Column8": 13.5
-   },
-   {
-    "OffersNo": 1486516,
-    "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Après-shampooing 250ml",
-    "category": "Cheveux ",
-    "Column4": "Après-Shampooing",
-    "PrixRegulier": 22,
-    "PrixResultatOffre": 12.5,
-    "Column8": 9.5
-   },
- 
- 
-   {
-    "OffersNo": 1472531,
-    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Stay Strong ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 21,
-    "Column8": 14
-   },
-   {
-    "OffersNo": 1472536,
-    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Healthy Hydration ",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 21,
-    "Column8": 14
-   },
-   {
-    "OffersNo": 1472534,
-    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Refresh Revitalize",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 21,
-    "Column8": 14
-   },
-   {
-    "OffersNo": 1472533,
-    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Nourish & Smooth",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 21,
-    "Column8": 14
-   },
-   {
-    "OffersNo": 1472532,
-    "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Full Volume",
-    "category": "Cheveux ",
-    "Column4": "Shampooing",
-    "PrixRegulier": 35,
-    "PrixResultatOffre": 21,
-    "Column8": 14
-   }
+      "OffersNo": 1551367,
+      "DescriptionOffre": "Advance techniques Mega Volume : Shampooing 400ml ",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 32,
+      "PrixResultatOffre": 20
+     },
+     {
+      "OffersNo": 1550668,
+      "DescriptionOffre": "Avon Kids Little Monster : Pastèque Shampooing et après Shampooing 200ml",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 30,
+      "PrixResultatOffre": 15
+     },
+     {
+      "OffersNo": 1550669,
+      "DescriptionOffre": "Avon Kids Little Monster : Pomme Shampooing et après Shampooing 200ml",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 30,
+      "PrixResultatOffre": 15
+     },
+     {
+      "OffersNo": 1550727,
+      "DescriptionOffre": "Avon Kids Fresh Scent : \"2en1\" Shampooing et Gel-douche 200ml",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 30,
+      "PrixResultatOffre": 15
+     },
+     {
+      "OffersNo": 1550725,
+      "DescriptionOffre": "Avon Kids Floral Scent : \"2en1\" Shampooing et après Shampooing 200ml",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 30,
+      "PrixResultatOffre": 15
+     },
+     {
+      "OffersNo": 1390759,
+      "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Shampooing 700ml ",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 38,
+      "PrixResultatOffre": 24
+     },
+     {
+      "OffersNo": 1390760,
+      "DescriptionOffre": "Advance techniques Ultimate Shine : Shampooing 700ml ",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 38,
+      "PrixResultatOffre": 24
+     },
+     {
+      "OffersNo": 1390758,
+      "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Shampooing 700ml ",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 38,
+      "PrixResultatOffre": 24
+     }
 ]
 export default Chmp
