df7faa6dc6c75154787ffbeea5611318d969028f
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-12-02 17:29:00 +0100

    Update Shmp.js II

--- client/src/Componenets/Products/category/cheveux/shmpJson/Shmp.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/Shmp.js
@@ -1,6 +1,5 @@
 const Chmp = [
   
-  
      {
       "OffersNo": 1550668,
       "DescriptionOffre": "Avon Kids Little Monster : Pastèque Shampooing et après Shampooing 200ml",
@@ -25,6 +24,55 @@ const Chmp = [
       "PrixRegulier": 30,
       "PrixResultatOffre": 15
      },
-   
+ 
+     {
+      "OffersNo": 1550725,
+      "DescriptionOffre": "Avon Kids Floral Scent : \"2en1\" Shampooing et après Shampooing 200ml",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 30,
+      "PrixResultatOffre": 0
+     },
+ 
+     {
+      "OffersNo": 1472531,
+      "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Stay Strong ",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 35,
+      "PrixResultatOffre": 22
+     },
+     {
+      "OffersNo": 1472536,
+      "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Healthy Hydration ",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 35,
+      "PrixResultatOffre": 22
+     },
+     {
+      "OffersNo": 1472534,
+      "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Refresh Revitalize",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 35,
+      "PrixResultatOffre": 22
+     },
+     {
+      "OffersNo": 1472533,
+      "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Nourish & Smooth",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 35,
+      "PrixResultatOffre": 22
+     },
+     {
+      "OffersNo": 1472532,
+      "DescriptionOffre": "Avon Care Shampooing et Après-Shampooing 2en1 700ml : Full Volume",
+      "category": "Cheveux ",
+      "Column4": "Shampooing",
+      "PrixRegulier": 35,
+      "PrixResultatOffre": 22
+     }
 ]
 export default Chmp
