9debf5dba4b7a3693742a7016ee0c10c15bcf424
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-12-02 18:09:19 +0100

    Update Homme.js II

--- client/src/Componenets/Products/category/homme/jsonHomme/Homme.js
+++ client/src/Componenets/Products/category/homme/jsonHomme/Homme.js
@@ -1,37 +1,88 @@
-const Homme = [ 
+const Homme = [
 
-  {
-    "OffersNo": 1505704,
-    "DescriptionOffre": "Fragrance Homme \" Attraction Awaken\" : Eau de toilette 75ml",
+  
+   {
+    "OffersNo": 1550889,
+    "DescriptionOffre": "Attraction Closer pour Lui : Eau de Toilette 75ml",
     "category": "Homme",
     "Column4": "Fragrance",
     "PrixRegulier": 134,
-    "PrixResultatOffre": 69
+    "PrixResultatOffre": 85
+   },
+   
+   {
+    "OffersNo": 1490717,
+    "DescriptionOffre": "Fragrance Homme \"Segno\" : Eau de parfum 75ml",
+    "category": "Homme",
+    "Column4": "Fragrance",
+    "PrixRegulier": 134,
+    "PrixResultatOffre": 79
    },
    {
-    "OffersNo": 1524283,
-    "DescriptionOffre": "Fragrance Homme \"Full Speed Pulse\" : Eau de toilette 75ml",
+    "OffersNo": 1490322,
+    "DescriptionOffre": "Fragrance Homme \"Luck\" : Eau de toilette 75ml",
+    "category": "Homme",
+    "Column4": "Fragrance",
+    "PrixRegulier": 134,
+    "PrixResultatOffre": 67
+   },
+   {
+    "OffersNo": 1475674,
+    "DescriptionOffre": "Fragrance Homme \"Full Speed Quantum\" : Gel douche 2en1 250ml",
+    "category": "Homme",
+    "Column4": "Fragrance",
+    "PrixRegulier": 33,
+    "PrixResultatOffre": 0
+   },
+   {
+    "OffersNo": 1460790,
+    "DescriptionOffre": "Fragrance Homme \"Full Speed\" : Déodorant à bille 50ml",
+    "category": "Homme",
+    "Column4": "Fragrance",
+    "PrixRegulier": 12,
+    "PrixResultatOffre": 0
+   },
+   {
+    "OffersNo": 1491565,
+    "DescriptionOffre": "Fragrance Homme \"Full Speed Surfer\" : Eau de toilette 75ml",
     "category": "Homme",
     "Column4": "Fragrance",
     "PrixRegulier": 124,
-    "PrixResultatOffre": 73
+    "PrixResultatOffre": 0
    },
    {
-    "OffersNo": 1491578,
-    "DescriptionOffre": "Fragrance Homme \"Black Suede Secret\" : Eau de toilette 75ml",
+    "OffersNo": 1491034,
+    "DescriptionOffre": "Fragrance Homme \"Full Speed\" : Eau de toilette 75ml",
     "category": "Homme",
     "Column4": "Fragrance",
-    "PrixRegulier": 116,
-    "PrixResultatOffre": 68
+    "PrixRegulier": 124,
+    "PrixResultatOffre": 0
+   },
+  
+   {
+    "OffersNo": 1454747,
+    "DescriptionOffre": "Fragrance Homme \"Full Speed Quantum\" : Déodorant à bille 50ml",
+    "category": "Homme",
+    "Column4": "Fragrance",
+    "PrixRegulier": 12,
+    "PrixResultatOffre": 0
    },
    {
-    "OffersNo": 1523550,
-    "DescriptionOffre": "Fragrance Homme \"Coffret Cadeau\" (EDT 75ml + HBW 250ml) : Individual Blue",
+    "OffersNo": 1552751,
+    "DescriptionOffre": "Attraction Closer pour Lui : Gel douche 2en1 200ml",
     "category": "Homme",
     "Column4": "Fragrance",
-    "PrixRegulier": 130,
-    "PrixResultatOffre": 63
+    "PrixRegulier": 33,
+    "PrixResultatOffre": 21
    },
+  //  {
+  //   "OffersNo": 1550251,
+  //   "DescriptionOffre": "Fragrance Homme \"Coffret Cadeau\" (EDT 75ml + HBW 200ml) : TTA Today",
+  //   "category": "Homme",
+  //   "Column4": "Fragrance",
+  //   "PrixRegulier": 188,
+  //   "PrixResultatOffre": 92
+  //  },
   //  {
   //   "OffersNo": 1550169,
   //   "DescriptionOffre": "Wild Country Coffret Cadeau",
@@ -48,119 +99,105 @@ const Homme = [
   //   "PrixRegulier": 164,
   //   "PrixResultatOffre": 79
   //  },
-  //  {
-  //   "OffersNo": 1550251,
-  //   "DescriptionOffre": "Fragrance Homme \"Coffret Cadeau\" (EDT 75ml + HBW 200ml) : TTA Today",
-  //   "category": "Homme",
-  //   "Column4": "Fragrance",
-  //   "PrixRegulier": 188,
-  //   "PrixResultatOffre": 92
-  //  },
    {
-    "OffersNo": 1491023,
-    "DescriptionOffre": "Fragrance Homme \"Elite Gentelmen\" : Eau de toilette 75ml",
+    "OffersNo": 1541524,
+    "DescriptionOffre": "Fragrance Homme \"Full Speed Boost\" : Déodorant à bille 50ml",
     "category": "Homme",
     "Column4": "Fragrance",
-    "PrixRegulier": 134,
-    "PrixResultatOffre": 59
+    "PrixRegulier": 12,
+    "PrixResultatOffre": 0
    },
   //  {
-  //   "OffersNo": 1519170,
-  //   "DescriptionOffre": "Fragrance Homme \"Elite Gentelman Quest\" : Eau de toilette 75ml",
+  //   "OffersNo": 1540180,
+  //   "DescriptionOffre": "Fragrance Homme \"Full Speed Boost \" : Eau de toilette 75ml",
+  //   "category": "Homme",
+  //   "Column4": "Fragrance",
+  //   "PrixRegulier": 124,
+  //   "PrixResultatOffre": 0
+  //  },
+  //  {
+  //   "OffersNo": 1537520,
+  //   "DescriptionOffre": "Fragrance Homme \"Full Speed Boost \" : Gel douche 250ml",
+  //   "category": "Homme",
+  //   "Column4": "Fragrance",
+  //   "PrixRegulier": 33,
+  //   "PrixResultatOffre": 0
+  //  },
+  //  {
+  //   "OffersNo": 1524909,
+  //   "DescriptionOffre": "Fragrance Homme \" Perceive\" : Eau de toilette 100ml",
   //   "category": "Homme",
   //   "Column4": "Fragrance",
   //   "PrixRegulier": 134,
-  //   "PrixResultatOffre": 59
+  //   "PrixResultatOffre": 0
   //  },
    {
-    "OffersNo": 1454746,
-    "DescriptionOffre": "Fragrance Homme \"Full Speed Quantum\" : Eau de toilette 75ml",
+    "OffersNo": 1523550,
+    "DescriptionOffre": "Fragrance Homme \"Coffret Cadeau\" (EDT 75ml + HBW 250ml) : Individual Blue",
     "category": "Homme",
     "Column4": "Fragrance",
-    "PrixRegulier": 124,
-    "PrixResultatOffre": 73
+    "PrixRegulier": 130,
+    "PrixResultatOffre": 63
    },
-   {
-    "OffersNo": 1475620,
-    "DescriptionOffre": "Fragrance Homme \"Black Suede Secret\" : Déodorant à bille 50ml",
-    "category": "Homme",
-    "Column4": "Fragrance",
-    "PrixRegulier": 12,
-    "PrixResultatOffre": 6
-   },
-   {
-    "OffersNo": 1454747,
-    "DescriptionOffre": "Fragrance Homme \"Full Speed Quantum\" : Déodorant à bille 50ml",
-    "category": "Homme",
-    "Column4": "Fragrance",
-    "PrixRegulier": 12,
-    "PrixResultatOffre": 6
-   },
+  //  {
+  //   "OffersNo": 1512072,
+  //   "DescriptionOffre": "Fragrance Homme \"Musk Metropolitano\" : Eau de toilette 75ml",
+  //   "category": "Homme",
+  //   "Column4": "Fragrance",
+  //   "PrixRegulier": 96,
+  //   "PrixResultatOffre": 0
+  //  },
    {
     "OffersNo": 1512071,
     "DescriptionOffre": "Fragrance Homme \"Musk Metropolitano\" : Déodorant à bille 50ml",
     "category": "Homme",
     "Column4": "Fragrance",
     "PrixRegulier": 12,
-    "PrixResultatOffre": 6
+    "PrixResultatOffre": 0
    },
-    {
-     "OffersNo": 1541524,
-     "DescriptionOffre": "Fragrance Homme \"Full Speed Boost\" : Déodorant à bille 50ml",
-     "category": "Homme",
-     "Column4": "Fragrance",
-     "PrixRegulier": 12,
-     "PrixResultatOffre": 6
-    },
+  
    {
-    "OffersNo": 1460790,
-    "DescriptionOffre": "Fragrance Homme \"Full Speed\" : Déodorant à bille 50ml",
+    "OffersNo": 1502905,
+    "DescriptionOffre": "Fragrance Homme \"Full Speed Electric \" : Gel douche corps et cheveux 200ml",
     "category": "Homme",
     "Column4": "Fragrance",
-    "PrixRegulier": 12,
-    "PrixResultatOffre": 6
+    "PrixRegulier": 33,
+    "PrixResultatOffre": 0
    },
    {
-    "OffersNo": 1522584,
-    "DescriptionOffre": "Fragrance Homme \"Black Suede Real\" : Déodorant à bille 50ml",
+    "OffersNo": 1493874,
+    "DescriptionOffre": "Attraction Intense pour Lui : Eau de Toilette 75ml",
     "category": "Homme",
     "Column4": "Fragrance",
-    "PrixRegulier": 12,
-    "PrixResultatOffre": 6
+    "PrixRegulier": 134,
+    "PrixResultatOffre": 79
    },
    {
-    "OffersNo": 1522352,
-    "DescriptionOffre": "Fragrance Homme \"Full Speed Pulse\" : Déodorant à bille 50ml",
+    "OffersNo": 1493848,
+    "DescriptionOffre": "Fragrance Homme \" Attraction\" : Eau de toilette 75ml",
     "category": "Homme",
     "Column4": "Fragrance",
-    "PrixRegulier": 12,
-    "PrixResultatOffre": 6
+    "PrixRegulier": 134,
+    "PrixResultatOffre": 79
    },
+   {
+    "OffersNo": 1491024,
+    "DescriptionOffre": "Fragrance Homme \"Elite Gentelmen In Black\" : Eau de toilette 75ml",
+    "category": "Homme",
+    "Column4": "Fragrance",
+    "PrixRegulier": 134,
+    "PrixResultatOffre": 67
+   },
+  
+  
+ 
    {
     "OffersNo": 1306421,
     "DescriptionOffre": "Fragrance Homme \"Individual Blue\" : Déodorant à bille 50ml",
     "category": "Homme",
     "Column4": "Fragrance",
     "PrixRegulier": 12,
-    "PrixResultatOffre": 6
-   },
-   {
-    "OffersNo": 1444791,
-    "DescriptionOffre": "Fragrance Homme \"Musk Marine\" : Déodorant à bille 50ml",
-    "category": "Homme",
-    "Column4": "Fragrance",
-    "PrixRegulier": 12,
-    "PrixResultatOffre": 6
-   },
-   {
-    "OffersNo": 1484904,
-    "DescriptionOffre": "Fragrance Homme \"Musk Instinct+\" : Déodorant à bille 50ml",
-    "category": "Homme",
-    "Column4": "Fragrance",
-    "PrixRegulier": 12,
-    "PrixResultatOffre": 6
-   },
- 
-  
-    ]
+    "PrixResultatOffre": 0
+   }
+]
   export default Homme
