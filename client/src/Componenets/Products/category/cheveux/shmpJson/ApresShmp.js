1b1dedce22207f77544f00839081d58140420040
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-12-02 17:44:53 +0100

    Update ApresShmp.js II

--- client/src/Componenets/Products/category/cheveux/shmpJson/ApresShmp.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/ApresShmp.js
@@ -1,19 +1,4 @@
 const ApresShmp = [
-  {
-    "OffersNo": 1551368,
-    "DescriptionOffre": "Advance techniques Mega Volume : Après-shampooing 250ml ",
-    "category": "Cheveux ",
-    "Column4": "Après-Shampooing",
-    "PrixRegulier": 22,
-    "PrixResultatOffre": 14
-   },
-   {
-    "OffersNo": 1486516,
-    "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Après-shampooing 250ml",
-    "category": "Cheveux ",
-    "Column4": "Après-Shampooing",
-    "PrixRegulier": 22,
-    "PrixResultatOffre": 14
-   }
+ 
 ]
 export default ApresShmp
