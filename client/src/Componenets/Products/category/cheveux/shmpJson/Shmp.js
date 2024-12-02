5b77ae21485d4fc52bb58cabfabd6bf1d4dfb8a1
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-12-02 17:23:08 +0100

    Update Shmp.js champ

--- client/src/Componenets/Products/category/cheveux/shmpJson/Shmp.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/Shmp.js
@@ -1,13 +1,6 @@
 const Chmp = [
   
-   {
-      "OffersNo": 1551367,
-      "DescriptionOffre": "Advance techniques Mega Volume : Shampooing 400ml ",
-      "category": "Cheveux ",
-      "Column4": "Shampooing",
-      "PrixRegulier": 32,
-      "PrixResultatOffre": 20
-     },
+  
      {
       "OffersNo": 1550668,
       "DescriptionOffre": "Avon Kids Little Monster : Pastèque Shampooing et après Shampooing 200ml",
@@ -32,37 +25,6 @@ const Chmp = [
       "PrixRegulier": 30,
       "PrixResultatOffre": 15
      },
-     {
-      "OffersNo": 1550725,
-      "DescriptionOffre": "Avon Kids Floral Scent : \"2en1\" Shampooing et après Shampooing 200ml",
-      "category": "Cheveux ",
-      "Column4": "Shampooing",
-      "PrixRegulier": 30,
-      "PrixResultatOffre": 15
-     },
-     {
-      "OffersNo": 1390759,
-      "DescriptionOffre": "Advance techniques Reconstitution à la kératine : Shampooing 700ml ",
-      "category": "Cheveux ",
-      "Column4": "Shampooing",
-      "PrixRegulier": 38,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1390760,
-      "DescriptionOffre": "Advance techniques Ultimate Shine : Shampooing 700ml ",
-      "category": "Cheveux ",
-      "Column4": "Shampooing",
-      "PrixRegulier": 38,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1390758,
-      "DescriptionOffre": "Advance techniques Huile d'argan du maroc : Shampooing 700ml ",
-      "category": "Cheveux ",
-      "Column4": "Shampooing",
-      "PrixRegulier": 38,
-      "PrixResultatOffre": 24
-     }
+   
 ]
 export default Chmp
