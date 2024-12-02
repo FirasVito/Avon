fe4ee6e9983426a9d06eeeafe87644db760b2ebe
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-12-02 18:10:57 +0100

    Update HommeToilette.js III

--- client/src/Componenets/Products/category/homme/jsonHomme/HommeToilette.js
+++ client/src/Componenets/Products/category/homme/jsonHomme/HommeToilette.js
@@ -1,542 +1,59 @@
 const HommeToilette = [
    {
-      "OffersNo": 1469186,
-      "DescriptionOffre": "Anew Reversalist : Crème de jour SPF 25 50ml",
-      "category": "Homme et Femme",
+      "OffersNo": 1372312,
+      "DescriptionOffre": "Avon Care Men Essential : Gel de Rasage 150ml",
+      "category": "Homme",
       "Column4": "Soins du visage",
-      "PrixRegulier": 132,
-      "PrixResultatOffre": 56
-     },
-
-     {
-      "OffersNo": 1387108,
-      "DescriptionOffre": "Anew Reversalist : Crème de nuit 50ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 132,
-      "PrixResultatOffre": 56
-     },
-     {
-      "OffersNo": 1387105,
-      "DescriptionOffre": "Anew Ultimate : Crème de jour SPF 25 50ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 152,
-      "PrixResultatOffre": 65
-     },
-     {
-      "OffersNo": 1387103,
-      "DescriptionOffre": "Anew Ultimate : Crème de nuit 50ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 152,
-      "PrixResultatOffre": 65
-     },
-     {
-      "OffersNo": 1387101,
-      "DescriptionOffre": "Anew Platinum : Crème de jour SPF 25 50ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 158,
-      "PrixResultatOffre": 67
-     },
-     {
-      "OffersNo": 1387102,
-      "DescriptionOffre": "Anew Platinum : Crème de nuit 50ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 158,
-      "PrixResultatOffre": 67
-     },
-     {
-      "OffersNo": 1334439,
-      "DescriptionOffre": "Anew Nettoyant : Gel Nettoyant 150ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 60,
-      "PrixResultatOffre": 29
-     },
-     {
-      "OffersNo": 1490243,
-      "DescriptionOffre": "Anew Sensitive+ : Crème nettoyante visage 150ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du visage",
-      "PrixRegulier": 60,
-      "PrixResultatOffre": 29
-     },
-     {
-      "OffersNo": 1344969,
-      "DescriptionOffre": "Anew Nettoyant : Eau Micellaire 200ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 66,
-      "PrixResultatOffre": 32
-     },
-     {
-      "OffersNo": 1387168,
-      "DescriptionOffre": "Anew Vitamine C :  Sérum Eclat 30ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 132,
-      "PrixResultatOffre": 58
-     },
-     {
-      "OffersNo": 1473643,
-      "DescriptionOffre": "Anew Vitamine C : Crème SPF50 50ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du visage",
-      "PrixRegulier": 122,
-      "PrixResultatOffre": 53
-     },
-     {
-      "OffersNo": 1533934,
-      "DescriptionOffre": "Anew Vitamine C : Crème pour les yeux correction éclat et cernes 15ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 62,
-      "PrixResultatOffre": 27
-     },
-     {
-      "OffersNo": 1509892,
-      "DescriptionOffre": "Anew Vitamine C Microdermabrasion : Gommage pour le visage 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 84,
-      "PrixResultatOffre": 36
-     },
-     {
-      "OffersNo": 1317970,
-      "DescriptionOffre": "Clearskin Blackhead Clearing : Masque  75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du visage",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 23
-     },
-     {
-      "OffersNo": 6351800,
-      "DescriptionOffre": "Clearskin Blackhead Clearing Tonique Clarifiant et Rafraîchissant 100ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du visage",
-      "PrixRegulier": 24,
-      "PrixResultatOffre": 13
-     },
-     {
-      "OffersNo": 1327038,
-      "DescriptionOffre": "Clearskin Pore & Shine Control : Savonnette 75gr",
-      "category": "Homme et Femme",
-      "Column4": "Soin",
-      "PrixRegulier": 20,
-      "PrixResultatOffre": 11
-     },
-     {
-      "OffersNo": 1497410,
-      "DescriptionOffre": "Clearskin Blemish : Triple Exfoliant à  l'Argile rose 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du visage",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 23
-     },
-     {
-      "OffersNo": 1400405,
-      "DescriptionOffre": "Clearskin Pore & Shine Control : Tonique 100ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du visage",
-      "PrixRegulier": 24,
-      "PrixResultatOffre": 13
-     },
-     {
-      "OffersNo": 1539354,
-      "DescriptionOffre": "Clearskin Pore & Shine Control : Gelée Nettoyante Purifiante 125ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du visage",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 23
-     },
-     {
-      "OffersNo": 4538900,
-      "DescriptionOffre": "Clearskin Accessoire : Tire-comédons 1pièce",
-      "category": "Homme et Femme",
-      "Column4": "Soin",
-      "PrixRegulier": 18,
-      "PrixResultatOffre": 8.5
-     },
-     {
-      "OffersNo": 1539353,
-      "DescriptionOffre": "Clearskin Blackhead: Exfoliant & Nettoyant 125ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du visage",
-      "PrixRegulier": 33,
-      "PrixResultatOffre": 16.5
-     },
-     {
-      "OffersNo": 1485854,
-      "DescriptionOffre": "Avon Care Huile de coco : Crème multi-usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1485856,
-      "DescriptionOffre": "Avon Care Grenade : Crème multi-usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1532054,
-      "DescriptionOffre": "Avon Care Avocado : Crème multi-usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1532057,
-      "DescriptionOffre": "Avon Care Beurre de cacao : Crème multi-usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1480993,
-      "DescriptionOffre": "Avon Care Gentle : Crème multi-usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1499739,
-      "DescriptionOffre": "Avon Care Essential Moisture : Crème Hydratante Multi-Usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1503939,
-      "DescriptionOffre": "Avon Care Berry Fusion : Crème multi-usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1531601,
-      "DescriptionOffre": "Avon Care Fruits Tropicaux : Crème multi-usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1541669,
-      "DescriptionOffre": "Avon Care Essential Express : Crème Hydratante Multi-Usages 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 42,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1541663,
-      "DescriptionOffre": "Avon Care Essential Express :  Lait hydratant pour le corps 400ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 18
-     },
-     {
-      "OffersNo": 1541667,
-      "DescriptionOffre": "Avon Care Essential Express : Gel de Corps 200ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 36,
-      "PrixResultatOffre": 16
-     },
-     {
-      "OffersNo": 1541668,
-      "DescriptionOffre": "Avon Care Essential Express : Crème mains 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 15,
-      "PrixResultatOffre": 6.5
-     },
-     {
-      "OffersNo": 1543695,
-      "DescriptionOffre": "Avon Care Essential Express : Brume Parfumée 250ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 56,
-      "PrixResultatOffre": 25
-     },
-     {
-      "OffersNo": 1542717,
-      "DescriptionOffre": "Footworks Huile de Coco et Citron : Exfoliant pour les pieds 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 28,
-      "PrixResultatOffre": 12.5
-     },
-     {
-      "OffersNo": 1542492,
-      "DescriptionOffre": "Footworks Huile de Coco et Citron : Crème hydratante pour les pieds 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 28,
-      "PrixResultatOffre": 12.5
-     },
-     {
-      "OffersNo": 1542493,
-      "DescriptionOffre": "Footworks Huile de Coco et Citron : Spray Rafraichissant pour Pieds 100ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
       "PrixRegulier": 34,
-      "PrixResultatOffre": 15
-     },
-     {
-      "OffersNo": 1466568,
-      "DescriptionOffre": "Senses Crème de douche 500ml : Simply Luxurious",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
       "PrixResultatOffre": 17
      },
      {
-      "OffersNo": 1458495,
-      "DescriptionOffre": "Senses Gel douche 500ml : Ocean Surge \"2en1\"",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1499370,
-      "DescriptionOffre": "Senses Gel de douche 500ml : Water Mint",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1519589,
-      "DescriptionOffre": "Senses Gel douche 500ml : Power Fresh",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1552105,
-      "DescriptionOffre": "Senses Crème de douche 500ml : Cosy Cashmere",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1458497,
-      "DescriptionOffre": "Senses Gel douche 500ml : Secret Lagoon",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1527590,
-      "DescriptionOffre": "Senses Crème de douche 500ml : L'amour Sunrise",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1534996,
-      "DescriptionOffre": "Senses Gel de douche 500ml : Tropical Mexicana",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1458826,
-      "DescriptionOffre": "Senses Gel douche 500ml : Pure Marine",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1499361,
-      "DescriptionOffre": "Senses Gel douche 500ml : Spiced Pepper",
+      "OffersNo": 1371017,
+      "DescriptionOffre": "Avon Care Men Essential : Baume 100ml",
       "category": "Homme",
-      "Column4": "Fragrance",
-      "PrixRegulier": 29,
-      "PrixResultatOffre": 17
-     },
-     {
-      "OffersNo": 1527596,
-      "DescriptionOffre": "Senses Bain Mousse 1L : L'amour Sunrise",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1527605,
-      "DescriptionOffre": "Senses Bain Mousse 1L : Aloha Monoï ",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1502051,
-      "DescriptionOffre": "Senses Bain Mousse 1L : Water Mint",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1542519,
-      "DescriptionOffre": "Senses Bain Mousse 1L : Sun Kissed Sunsets",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1534991,
-      "DescriptionOffre": "Senses Bain Mousse 1L : Tropical Mexicana",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1530675,
-      "DescriptionOffre": "Senses Bain Mousse 1L : Lemon Brust",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1527623,
-      "DescriptionOffre": "Senses Bain Mousse 1L : Lavender Calm",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1527619,
-      "DescriptionOffre": "Senses Bain Mousse 1L : Simply Luxurious",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1552106,
-      "DescriptionOffre": "Senses Bain Mousse 1L : Cosy Cashmere",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 40,
-      "PrixResultatOffre": 24
-     },
-     {
-      "OffersNo": 1439357,
-      "DescriptionOffre": "On duty déo à bille Lui : Active 50ml",
-      "category": "Homme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 12,
-      "PrixResultatOffre": 7
-     },
-     {
-      "OffersNo": 1317279,
-      "DescriptionOffre": "On duty déo à bille Elle : Active 50ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du corps ",
-      "PrixRegulier": 12,
-      "PrixResultatOffre": 7
+      "Column4": "Soins du visage",
+      "PrixRegulier": 26,
+      "PrixResultatOffre": 13
      },
      {
       "OffersNo": 1444819,
       "DescriptionOffre": "On duty déo à bille Lui : Invisible 50ml",
       "category": "Homme",
-      "Column4": "Soins du corps",
+      "Column4": "Soins du Corps",
       "PrixRegulier": 12,
-      "PrixResultatOffre": 7
+      "PrixResultatOffre": 7.5
      },
      {
-      "OffersNo": 4624000,
-      "DescriptionOffre": "On duty déo à bille Elle : Invisible 50ml",
-      "category": "Homme et Femme",
-      "Column4": "Soin du corps ",
+      "OffersNo": 1439357,
+      "DescriptionOffre": "On duty déo à bille Lui : Active 50ml",
+      "category": "Homme",
+      "Column4": "Soins du Corps",
       "PrixRegulier": 12,
-      "PrixResultatOffre": 7
+      "PrixResultatOffre": 7.5
+     },
+     {
+      "OffersNo": 1380774,
+      "DescriptionOffre": "Avon Care Men Essential : Shampooing, Après-shampooing, Gel-douche 200ml",
+      "category": "Homme",
+      "Column4": "Soins du visage",
+      "PrixRegulier": 28,
+      "PrixResultatOffre": 14
+     },
+     {
+      "OffersNo": 1508436,
+      "DescriptionOffre": "Avon Care Men Essential : Gel pour le visage avant rasage, après rasage et hydratant 50ml",
+      "category": "Homme",
+      "Column4": "Soins du visage",
+      "PrixRegulier": 34,
+      "PrixResultatOffre": 17
      },
      {
       "OffersNo": 4626700,
       "DescriptionOffre": "On duty déo à bille Lui : Max Protection 50ml",
       "category": "Homme",
-      "Column4": "Soins du corps",
+      "Column4": "Soins du Corps",
       "PrixRegulier": 12,
-      "PrixResultatOffre": 7
+      "PrixResultatOffre": 7.5
      },
-     {
-      "OffersNo": 1486534,
-      "DescriptionOffre": "Footworks : Crème pour Talons très Fendillés 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 28,
-      "PrixResultatOffre": 16
-     },
-     {
-      "OffersNo": 6305500,
-      "DescriptionOffre": "Footworks : Crème  cors et les callosités 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 28,
-      "PrixResultatOffre": 16
-     },
-     {
-      "OffersNo": 1534226,
-      "DescriptionOffre": "Footworks : Crème Hydratante Soin Intense 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 28,
-      "PrixResultatOffre": 16
-     },
-     {
-      "OffersNo": 1530520,
-      "DescriptionOffre": "Footworks : Soin Peau Rugueuse 75ml",
-      "category": "Homme et Femme",
-      "Column4": "Soins du corps",
-      "PrixRegulier": 28,
-      "PrixResultatOffre": 16
-     },
-   
-     {
-      "OffersNo": 1523550,
-      "DescriptionOffre": "Fragrance Homme \"Coffret Cadeau\" (EDT 75ml + HBW 250ml) : Individual Blue",
-      "category": "Homme",
-      "Column4": "Fragrance",
-      "PrixRegulier": 130,
-      "PrixResultatOffre": 60
-     },
-     {
-      "OffersNo": 1527221,
-      "DescriptionOffre": "Soin \"Coffret cadeau\" (Sérum 30ml + Crème Contour Yeux 15ml) : Anew Renewal Power",
-      "category": "Homme et Femme",
-      "Column4": "Soins du visage",
-      "PrixRegulier": 180,
-      "PrixResultatOffre": 69
-     }
-   
 ]
   export default HommeToilette
