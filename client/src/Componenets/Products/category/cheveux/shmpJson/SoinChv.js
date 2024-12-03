bc607fca65f0b71853849b1ccb9d7171313504a3
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-12-03 14:58:52 +0100

    Update SoinChv.js

--- client/src/Componenets/Products/category/cheveux/shmpJson/SoinChv.js
+++ client/src/Componenets/Products/category/cheveux/shmpJson/SoinChv.js
@@ -1,6 +1,5 @@
 const SoinChv = [ 
 
-  
   {
     "OffersNo": 1552014,
     "DescriptionOffre": "Avon Kids Little Monster : Mangue Spray démêlant pour cheveux 200ml",
@@ -9,14 +8,7 @@ const SoinChv = [
     "PrixRegulier": 30,
     "PrixResultatOffre": 15
    },
-   {
-    "OffersNo": 1550728,
-    "DescriptionOffre": "Avon Kids Fresh Scent : Gel pour cheveux 50ml",
-    "category": "Cheveux ",
-    "Column4": "Soins",
-    "PrixRegulier": 30,
-    "PrixResultatOffre": 0
-   },
+
    {
     "OffersNo": 1550726,
     "DescriptionOffre": "Avon Kids Floral Scent : Spray démêlant pour cheveux 200ml",
