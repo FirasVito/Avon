396917e93b59ab75b24885e14aba2b5663bc9af8
FirasVito <165796022+FirasVito@users.noreply.github.com>
2024-12-03 15:01:24 +0100

    Update Enf.js

--- client/src/Componenets/Products/category/enfants/Enf.js
+++ client/src/Componenets/Products/category/enfants/Enf.js
@@ -15,14 +15,7 @@ const Enf = [
     "PrixRegulier": 30,
     "PrixResultatOffre": 15
    },
-   {
-    "OffersNo": 1550729,
-    "DescriptionOffre": "Avon Kids Fresh Scent : Eau de cologne 150ml",
-    "category": "Enfant",
-    "Column4": "Fragrance",
-    "PrixRegulier": 58,
-    "PrixResultatOffre": 0
-   },
+
    {
     "OffersNo": 1551413,
     "DescriptionOffre": "Avon Kids Floral Scent : Gel douche 200ml",
@@ -31,14 +24,7 @@ const Enf = [
     "PrixRegulier": 30,
     "PrixResultatOffre": 15
    },
-   {
-    "OffersNo": 1555263,
-    "DescriptionOffre": "Avon Kids Floral Scent : Eau de cologne 30ml",
-    "category": "Enfant",
-    "Column4": "Fragrance",
-    "PrixRegulier": 84,
-    "PrixResultatOffre": 0
-   }
+
   
 ];
 
